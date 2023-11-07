bfd6cee005ddadce963ade90dada8d1e721bb76d
Divine Hycenth <divinehycenth@outlook.com>
2023-11-07 21:08:57 +0100

    more projects

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
     serverActions: true,
   },
   images: {
-    domains: ["res.cloudinary.com", "github.com"],
+    domains: [
+      "res.cloudinary.com",
+      "github.com",
+      "upwork-usw2-prod-agora-file-storage.s3.us-west-2.amazonaws.com",
+    ],
   },
 };
 
--- src/app/works/works.ts
+++ src/app/works/works.ts
@@ -3,7 +3,7 @@ export const worksData = [
     title: "Flashpay",
     description:
       "Flashpay is a fintech company based in the Netherlands that offers A2A payment services to businesses in Africa and empowers banks by using existing bank infrastructure to facilitate payments outside open banking",
-    imageUrl: "/flashpay-website.gif",
+    imageUrl: "/project-images/flashpayweb.png",
     githubUrl: "https://github.com/nature-ui/nature-ui",
     websiteUrl: "https://flashpayweb.com",
     tags: ["#fintech", "#react", "#typescript"],
@@ -18,10 +18,19 @@ export const worksData = [
     tags: ["#fintech", "#react", "#typescript"],
   },
   {
+    title: "Fitness fiends",
+    description:
+      "Fitness Fiends is a collection of 6,666 fitness-loving fiends working out on the Ethereum blockchain. Each fiend doubles up as your membership to the Fitness Fiends ecosystem granting a whole array of real-world and digital benefits, from free gym membership to an immersive Metaverse where you can train, game and socialize. ",
+    imageUrl: "/project-images/fitnessfiends.png",
+    githubUrl: "",
+    websiteUrl: "http://fitnessfiends.com/",
+    tags: ["#NFT", "#solidity", "#react"],
+  },
+  {
     title: "Naeme",
     description:
       "Naeme is a mordern event management platform built with security, ease of use and data in mind.",
-    imageUrl: "/naeme-website.gif",
+    imageUrl: "/project-images/naeme.png",
     githubUrl: "https://github.com/nature-ui/nature-ui",
     websiteUrl: "https://naeme.app",
     tags: ["#open-source", "#react", "#typescript"],
@@ -34,6 +43,16 @@ export const worksData = [
     githubUrl: "#",
     websiteUrl: "https://earlybean.co/",
     tags: ["#open-source", "#react", "#typescript"],
+  },
+  {
+    title: "Multidextrous token (MDEFI)",
+    description:
+      "Multidextrous is a decentralized finance platform (DEFI) that focuses on optimizing your trades across hundreds of DEXes on multiple networks.",
+    imageUrl:
+      "https://upwork-usw2-prod-agora-file-storage.s3.us-west-2.amazonaws.com/profile/portfolio/thumbnail/893de2a0a22acd3254594c8acb9df645?response-content-disposition=inline;+filename=%22image_large%22;+filename*=utf-8%27%27image_large&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEHQaCXVzLXdlc3QtMiJHMEUCICggrScPJIRq%2BIkcGusHlAWoN2GETgbjnfvtsh42SBfXAiEAxMeUiVYhSOQFy6SKb4DSNkkwG5aa60wGQ65%2BzPo2sqEq1gQIrf//////////ARAAGgw3Mzk5MzkxNzM4MTkiDDbih9lvNmBDu5xglSqqBKrlevkmwNWPQ/ytMJLftD7kDb/rKIFzk9obOfmwTUH%2B9PtN41f9t4UDoUZGcFakLvzwOKeIe2x71NIkc7VEcXd04HYIKRjjcxvl5V9rW5wsMbF1HKPc4Pyy%2BbZQp3h4mHveNumEL867MP/xJKuGd2kgJcPZ0Lxp963wShOwol1dGu%2BhmA5cheHwAfwYkjJW0n39fUypmWE0bSsBrz0kLzjCzX59IJ0zbmQMjhK0Jh/nW2mE0TCZlXPgdeRUNPmpP/1ezpo8kLoXOEhKMnK7kokeu6N2CayJwXAGM/OoRX/c%2BBvEQqCiXrTbDzr6bRRofH7Ezf2UG1YmdZ/oJ6gsGX90AIaEdOJw9d4zWGwrtYVtbpAC6Tf9wCIyk3l%2B%2BmJ/aa4odLcTZQWa44EN0mxuoeZY3yNx2vDH6u%2Bnu9VaSnh96f0mIauLPXN5rJol39dv6T0M/hiC%2BDSiF8hGLSTbQJyZR91kq6ezRtBaA3IkUjrjhuZOkLUWniLhw14Ij0YoM/MvlRGsLU6ROOEx6Nw8mwYm0Fd6o/uFWTFlBcZKhYsFz860WDZraDq3RBeE44%2B/mjZzdEOw3aIE9keWqE1GOionEUPF2rPuwBSKbvIEOXwnHuwXdLClbzIBtN96nm2O4bswqYh9cONHz7%2BgAfnSPlRAJkGj0MGOSXYSg4X5w6pbZPVesLHTh4xi0td8uIm0I%2BDga2FS5FGIWNelvtS7w7eEwXN6WERO64R/MPSsqqoGOqcBiDXafOoc29AVSwuVsE1H6ro3i/fLkmix2sUMFF/sQJt9ZR3TOcB2OnJtlq4VMtitRd/3npWTCMaD4WIapf9s0MSMOx6kse7O73m1p/h3d27d0LeK9UEnpdStnAwXzmhxDxvKMuLaDguRaAAqaa4/RicI0PJQgf4fqQTNwrALQG06IOoaRZnmrYSGLsLOV/dOhwA6JWwODMPW5HgkeX1ulq0Ee%2B8HxHU=&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20231107T200605Z&X-Amz-SignedHeaders=host&X-Amz-Expires=900&X-Amz-Credential=ASIA2YR6PYW56KHHQPU6/20231107/us-west-2/s3/aws4_request&X-Amz-Signature=c4ad61dd5cd930ef8c8dc0c1365c988a41e57dcadc088326fbefb6594c4ec3aa",
+    githubUrl: "",
+    websiteUrl: "http://mdefi.io/",
+    tags: ["#solidity", "#react"],
   },
   {
     title: "CombatCovid",
